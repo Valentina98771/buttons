ce80744526e835f902abe2c76d4199176927c7c2
Valentina98771 <76867909+Valentina98771@users.noreply.github.com>
2021-02-01 09:57:22 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,29 +1,29 @@
-let x, game, buttons, test
+let x, game, buttons, test;
 
 function myFunction() {
-     x = document.getElementById("number").value
-     game = Math.floor(Math.random() * x)
-     buttons = document.getElementsByTagName("div")[0]
+     x = document.getElementById("number").value;
+     game = Math.floor(Math.random() * x);
+     buttons = document.getElementsByTagName("div")[0];
 
     for (let i = 0; i < x; ++i) {
-        test = document.createElement("button")
-        test.innerHTML = 'An button'
-        test.style.color = "red"
-        test.style.padding = "12px 20px"
-        test.style.backgroundColor = "blue"
+        test = document.createElement("button");
+        test.innerHTML = 'An button';
+        test.style.color = "red";
+        test.style.padding = "12px 20px";
+        test.style.backgroundColor = "blue";
         buttons.appendChild(test);
-        if(i === game) {
+        if (i === game) {
             test.addEventListener ("click", function() {
-            alert("Hmm, good luck to you today.")
-            history.go(0)})
-        }
-        else {
+            alert("Hmm, good luck to you today.");
+            history.go(0)});
+        } else {
             test.addEventListener ("click", function() {
-            alert("Keep trying")})
+            alert("Keep trying")});
         }
     }
 }
 
 function notTry() {
-    alert("Guess the button, then try your luck at another game")
+    alert("Guess the button, then try your luck at another game");
 }
+
